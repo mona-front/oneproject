f65711bcd443062ae1131135405668fa5f809dee
mona behkerdar <monabehkerdar@gmail.com>
2021-03-23 12:27:26 +0430

    add gitignore

---
+++ min.js
@@ -0,0 +1,9 @@
+let portfolio=document.querySelectorAll('.portfolio-menu-item')
+let webmenu=document.querySelector('.web')
+let sectionitem=document.querySelectorAll('.portfolio-section-item')
+webmenu.addEventListener('click',fucntion(){
+	
+
+
+})
+
