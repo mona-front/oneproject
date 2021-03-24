125144bc91a1501f50efe788dd879647fff353ca
mona-front <monabehkerdar@gmail.com>
2021-03-24 12:54:54 +0430

    add file min.js

--- min.js
+++ min.js
@@ -1,9 +1,6 @@
-let portfolio=document.querySelectorAll('.portfolio-menu-item')
-let webmenu=document.querySelector('.web')
-let sectionitem=document.querySelectorAll('.portfolio-section-item')
-webmenu.addEventListener('click',fucntion(){
-	
-
-
-})
-
+let portfoliomenuitem=document.querySelectorAll('.portfolio-menu-item')
+let portfoliosectionitem=document.querySelectorAll('.portfolio-section-item')
+let web=document.querySelector('#web')
+	for(var i-0; i<portfoliosectionitem.length;i++){
+		portfoliosectionitem[i].style.display="none";
+	}
